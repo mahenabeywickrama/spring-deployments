60b0823695fe3d4b623c4b27e4dd81fab1fce714
Mahen Abeywickrama <mahenabeywickrema0@gmail.com>
2025-08-07 13:56:24 +0530

    added mapping

--- src/main/java/com/example/springdeployments/controller/CustomerController.java
+++ src/main/java/com/example/springdeployments/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/customer")
 @RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
